344add48e17610692274f36c5f6fe3f482b7ef39
7rvn <valentinhobusch@icloud.com>
2021-03-28 00:17:57 +0100

    learn with video demo implementation

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,8 @@
 import * as React from "react";
 
-import { Square, playSound, pieces } from "../utils/utils";
+import { Square, pieces, chessjsBoardToPositionObj } from "../utils/utils";
+import initialPositionObj from "../utils/initialPosition";
 
-// converts chess.js board representation to
-// array with string keys of rank+file
-function constructPosition(position) {
-  let positionObj = {};
-  position.forEach((rank, rankIndex) => {
-    rank.forEach((square, fileIndex) => {
-      positionObj[
-        Math.abs(rankIndex - 7).toString() + fileIndex.toString()
-      ] = square;
-    });
-  });
-  return positionObj;
-}
-
-// returns x, y position of mouse inside board div
-// returns rank, file of mouse on the board
 function getBoardPosition(e, board, orientation) {
   const factor = orientation === "white" ? 0 : 7;
   const x = ((e.clientX - board.offsetLeft) / board.offsetWidth) * 800;
@@ -30,276 +15,321 @@ function getBoardPosition(e, board, orientation) {
   return { x: x, y: y, rank: rank, file: file };
 }
 
-const Board = React.forwardRef(({ clickHandler, appHandleDragStart }, ref) => {
-  //console.log("render board");
-
-  /* Ref functions*/
-  /* ************ */
-  React.useImperativeHandle(ref, () => ({
-    // sets position state
-    setBoard(position) {
-      setPosition(constructPosition(position));
+const Board = React.forwardRef(
+  (
+    {
+      initialPosition,
+      initialOrientation = "white",
+      onMakeMove,
+      onActivatePiece,
     },
+    ref
+  ) => {
+    React.useImperativeHandle(ref, () => ({
+      /**
+       * sets board state
+       * @param {Object} position chessjs game.board() position
+       */
+      setBoard(position) {
+        setPosition(chessjsBoardToPositionObj(position));
+      },
 
-    // makes move sound
-    makeMove(move) {
-      playSound(move.flags);
-    },
-
-    // sets board orientation state
-    orientation(newOrientation) {
-      if (newOrientation !== orientation) {
-        setOrientation(newOrientation);
-      }
-    },
-
-    snapback() {
-      console.log("takeitback");
-    },
-  }));
-
-  /* States */
-  /* ************ */
-  const [position, setPosition] = React.useState({});
-  const [orientation, setOrientation] = React.useState("white");
-  const [boardSize, setBoardSize] = React.useState(
-    () => window.localStorage.getItem("boardSize") || 500
-  );
-
-  const [activeSquare, setActiveSquare] = React.useState();
-  const [hoverSquare, setHoverSquare] = React.useState();
-  const [legalSquares, setLegalSquares] = React.useState([]);
-  const [highlightSquares, setHighlightSquare] = React.useState({});
-
-  const boardRef = React.useRef();
-
-  // sets position state calcuted from move made
-  const setNewPosition = React.useCallback(
-    ({ from, to }) => {
-      const fromString = from.rank.toString() + from.file.toString();
-      const toString = to.rank.toString() + to.file.toString();
-      const newPosition = { ...position };
-
-      newPosition[fromString] = null;
-      newPosition[toString] = position[fromString];
-      setPosition(newPosition);
-    },
-    [position]
-  );
-
-  /* eventhandlers */
-  /* ************ */
-  const handleDragStart = React.useCallback(
-    (e) => {
-      if (e.button === 0) {
-        const { x, y, rank, file } = getBoardPosition(
-          e,
-          boardRef.current,
-          orientation
-        );
-
-        if (position[rank.toString() + file.toString()]) {
-          setLegalSquares(appHandleDragStart({ rank: rank, file: file }));
-          e.target.style.transform = `translate(${x - 50}%, ${y - 50}%)`;
-          e.target.style.zIndex = 100;
-          e.target.style.cursor = "grabbing";
-          setActiveSquare({ div: e.target, hex: { rank: rank, file: file } });
-          setHoverSquare({ rank: rank, file: file });
-        }
-      }
-    },
-    [appHandleDragStart, position, orientation]
-  );
-
-  const handleDrag = React.useCallback(
-    (e) => {
-      if (activeSquare) {
-        const { x, y, rank, file } = getBoardPosition(
-          e,
-          boardRef.current,
-          orientation
-        );
-
-        activeSquare.div.style.transform = `translate(${x - 50}%, ${y - 50}%)`;
-
-        if (
-          !hoverSquare ||
-          hoverSquare.rank !== rank ||
-          hoverSquare.file !== file
-        ) {
-          setHoverSquare({ rank: rank, file: file });
-        }
-      }
-    },
-    [activeSquare, hoverSquare, orientation]
-  );
-
-  const handleDragEnd = React.useCallback(
-    (e) => {
-      const { rank, file } = getBoardPosition(e, boardRef.current, orientation);
-
-      if (activeSquare) {
-        setActiveSquare();
-        activeSquare.div.removeAttribute("style");
-        // if drag ended on different square from where it started
-        if (activeSquare.hex.rank !== rank || activeSquare.hex.file !== file) {
-          let [validMove, moveAction] = clickHandler({
-            from: activeSquare.hex,
-            to: { rank: rank, file: file },
-          });
-
-          setLegalSquares([]);
-          setHoverSquare();
-          if (!validMove) {
-            return;
-          }
-
-          if (moveAction) {
-            setHighlightSquare({
-              ...highlightSquares,
-              [activeSquare.hex.rank.toString() +
-              activeSquare.hex.file.toString()]: "wrong-move",
-              [rank.toString() + file.toString()]: "wrong-move",
-            });
-            setTimeout(function () {
-              setHighlightSquare({ ...highlightSquares });
-              setPosition(position);
-            }, 1500);
-          }
-
-          playSound(validMove.flags);
-
-          setNewPosition({
-            from: activeSquare.hex,
-            to: { rank: rank, file: file },
-          });
-        }
-        // if drag ended on start square handle it like a click
-      } else {
-      }
-    },
-    [
-      activeSquare,
-      clickHandler,
-      setNewPosition,
-      orientation,
-      position,
-      highlightSquares,
-    ]
-  );
-
-  const handleKeyDown = React.useCallback(
-    (e) => {
-      if (e.key === "x") {
-        if (orientation === "white") {
-          setOrientation("black");
+      /**
+       * executes moves on the board and returns new position
+       * @param {[string, string][]} moves as array of hex values [from, to]
+       * @param {boolean} [animate] if moves have to be animated or instantly change the board
+       */
+      move(moves, animate = false) {
+        if (animate) {
         } else {
-          setOrientation("white");
         }
+      },
+
+      /**
+       * Sets boards orientation
+       * @param {string} newOrientation "black"|| "white"
+       */
+      orientation(newOrientation) {
+        if (newOrientation !== orientation) {
+          setOrientation(newOrientation);
+        }
+      },
+
+      addHighlights({ squares, type }) {
+        if (type === "legalMoves") {
+          setLegalMoves(squares);
+        }
+      },
+    }));
+
+    const [position, setPosition] = React.useState(() => {
+      if (initialPosition) {
+        chessjsBoardToPositionObj(initialPosition);
+      } else {
+        return initialPositionObj;
       }
-    },
-    [orientation]
-  );
+    });
 
-  const clickDefault = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-  };
+    const [orientation, setOrientation] = React.useState(initialOrientation);
+    const [boardSize, setBoardSize] = React.useState(
+      () => window.localStorage.getItem("boardSize") || 500
+    );
 
-  /* event listeners */
-  /* ************ */
-  React.useEffect(() => {
-    const b = boardRef.current;
-    b.addEventListener("mousedown", handleDragStart);
-    b.addEventListener("mousemove", handleDrag);
-    b.addEventListener("mouseup", handleDragEnd);
-    window.addEventListener("keydown", handleKeyDown);
-    return () => {
-      b.removeEventListener("mousedown", handleDragStart);
-      b.removeEventListener("mousemove", handleDrag);
-      b.removeEventListener("mouseup", handleDragEnd);
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [handleDragStart, handleDrag, handleDragEnd, handleKeyDown]);
+    const [activeSquare, setActiveSquare] = React.useState();
+    const [hoverSquare, setHoverSquare] = React.useState();
+    const [legalMoves, setLegalMoves] = React.useState();
 
-  React.useEffect(() => {
-    window.localStorage.setItem("boardSize", boardSize);
-  }, [boardSize]);
+    const boardRef = React.useRef();
 
-  let resizeX;
+    /**
+     * updates position state
+     * @param  from Object with .rank and .file as numbers
+     * @param  to Object with .rank and .file as numbers
+     */
+    const updatePosition = React.useCallback(
+      ({ from, to }) => {
+        const fromString = from.rank.toString() + from.file.toString();
+        const toString = to.rank.toString() + to.file.toString();
+        const newPosition = { ...position };
 
-  const initResize = (e) => {
-    resizeX = e.clientX;
+        newPosition[fromString] = null;
+        newPosition[toString] = position[fromString];
+        setPosition(newPosition);
+      },
+      [position]
+    );
 
-    document.addEventListener("mousemove", moveResize, false);
-    document.addEventListener("mouseup", stopResize, false);
-  };
+    const makeMove = React.useCallback(
+      ({ from, to, animate = false }) => {
+        if (onMakeMove) {
+          onMakeMove({
+            from: from,
+            to: to,
+          });
+        } else {
+          updatePosition({
+            from: from,
+            to: to,
+          });
+        }
 
-  const moveResize = (e) => {
-    setBoardSize(parseInt(boardSize) + (e.clientX - resizeX));
-  };
+        setActiveSquare();
+        setLegalMoves();
+      },
+      [onMakeMove, updatePosition]
+    );
 
-  const stopResize = (e) => {
-    document.removeEventListener("mousemove", moveResize, false);
-    document.removeEventListener("mouseup", stopResize, false);
-    console.log("done");
-  };
-
-  return (
-    <div
-      className={
-        orientation === "white" ? "board-layout" : "flipped board-layout"
-      }
-    >
-      <div
-        className="board"
-        id="board-board"
-        style={{ width: boardSize + "px", height: boardSize + "px" }}
-        ref={boardRef}
-        onContextMenu={clickDefault}
-      >
-        {pieces(position)}
-
-        {legalSquares.map((square) => {
-          return (
-            <Square
-              type={
-                square[1].includes("c") || square[1].includes("e")
-                  ? "capture-hint"
-                  : "hint"
-              }
-              square={square[0]}
-              key={square}
-            />
+    /* eventhandlers */
+    /* ************ */
+    const handleMouseDown = React.useCallback(
+      (e) => {
+        if (e.button === 0) {
+          const { x, y, rank, file } = getBoardPosition(
+            e,
+            boardRef.current,
+            orientation
           );
-        })}
 
-        {hoverSquare ? (
-          <Square
-            type={"hover-square"}
-            square={hoverSquare.rank.toString() + hoverSquare.file.toString()}
-          ></Square>
-        ) : null}
+          const clicked =
+            activeSquare?.hex.rank === rank && activeSquare?.hex.file === file
+              ? true
+              : false;
 
-        {activeSquare ? (
-          <Square
-            type={"highlight"}
-            square={
-              activeSquare.hex.rank.toString() +
-              activeSquare.hex.file.toString()
+          if (
+            activeSquare &&
+            (activeSquare.hex.rank !== rank || activeSquare.hex.file !== file)
+          ) {
+            makeMove({
+              from: activeSquare.hex,
+              to: { rank: rank, file: file },
+              animate: true,
+            });
+          }
+
+          if (
+            position[rank.toString() + file.toString()] &&
+            (!activeSquare || clicked)
+          ) {
+            e.target.style.transform = `translate(${x - 50}%, ${y - 50}%)`;
+            e.target.style.zIndex = 100;
+            e.target.style.cursor = "grabbing";
+            setActiveSquare({
+              div: e.target,
+              hex: { rank: rank, file: file },
+              clicked: clicked,
+              dragging: true,
+            });
+            setHoverSquare({ rank: rank, file: file });
+
+            if (onActivatePiece && !clicked) {
+              onActivatePiece({ rank: rank, file: file });
             }
-            style={{ backgroundColor: "rgba(255, 255, 0)" }}
-          ></Square>
-        ) : null}
+          }
+        }
+      },
+      [orientation, activeSquare, position, makeMove, onActivatePiece]
+    );
 
-        {Object.entries(highlightSquares).map((entry) => {
-          let [square, flag] = entry;
-          return <Square type={"highlight " + flag} square={square}></Square>;
-        })}
+    const handleMouseMove = React.useCallback(
+      (e) => {
+        if (activeSquare?.dragging) {
+          const { x, y, rank, file } = getBoardPosition(
+            e,
+            boardRef.current,
+            orientation
+          );
+
+          activeSquare.div.style.transform = `translate(${x - 50}%, ${
+            y - 50
+          }%)`;
+
+          if (
+            !hoverSquare ||
+            hoverSquare.rank !== rank ||
+            hoverSquare.file !== file
+          ) {
+            setHoverSquare({ rank: rank, file: file });
+          }
+        }
+      },
+      [activeSquare, hoverSquare, orientation]
+    );
+
+    const handleMouseUp = React.useCallback(
+      (e) => {
+        const { rank, file } = getBoardPosition(
+          e,
+          boardRef.current,
+          orientation
+        );
+        if (!activeSquare) {
+          return;
+        }
+        setHoverSquare();
+
+        if (activeSquare.hex.rank === rank && activeSquare.hex.file === file) {
+          activeSquare.div.removeAttribute("style");
+          if (activeSquare.clicked) {
+            setActiveSquare();
+            setLegalMoves();
+          } else {
+            setActiveSquare({ ...activeSquare, dragging: false });
+          }
+        } else {
+          activeSquare.div.removeAttribute("style");
+          makeMove({
+            from: activeSquare.hex,
+            to: { rank: rank, file: file },
+          });
+        }
+      },
+      [activeSquare, makeMove, orientation]
+    );
+
+    const handleKeyDown = React.useCallback(
+      (e) => {
+        if (e.key === "x") {
+          if (orientation === "white") {
+            setOrientation("black");
+          } else {
+            setOrientation("white");
+          }
+        }
+      },
+      [orientation]
+    );
+
+    const clickDefault = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+    };
+
+    /* event listeners */
+    /* ************ */
+    React.useEffect(() => {
+      const b = boardRef.current;
+      b.addEventListener("mousedown", handleMouseDown);
+      b.addEventListener("mousemove", handleMouseMove);
+      b.addEventListener("mouseup", handleMouseUp);
+      window.addEventListener("keydown", handleKeyDown);
+      return () => {
+        b.removeEventListener("mousedown", handleMouseDown);
+        b.removeEventListener("mousemove", handleMouseMove);
+        b.removeEventListener("mouseup", handleMouseUp);
+        window.removeEventListener("keydown", handleKeyDown);
+      };
+    }, [handleMouseDown, handleMouseMove, handleMouseUp, handleKeyDown]);
+
+    React.useEffect(() => {
+      window.localStorage.setItem("boardSize", boardSize);
+    }, [boardSize]);
+
+    let resizeX;
+
+    const initResize = (e) => {
+      resizeX = e.clientX;
+
+      document.addEventListener("mousemove", moveResize, false);
+      document.addEventListener("mouseup", stopResize, false);
+    };
+
+    const moveResize = (e) => {
+      setBoardSize(parseInt(boardSize) + (e.clientX - resizeX));
+    };
+
+    const stopResize = (e) => {
+      document.removeEventListener("mousemove", moveResize, false);
+      document.removeEventListener("mouseup", stopResize, false);
+      console.log("done");
+    };
+
+    return (
+      <div
+        className={
+          orientation === "white" ? "board-layout" : "flipped board-layout"
+        }
+      >
+        <div
+          className="board"
+          id="board-board"
+          style={{ width: boardSize + "px", height: boardSize + "px" }}
+          ref={boardRef}
+          onContextMenu={clickDefault}
+        >
+          {pieces(position)}
+
+          {hoverSquare ? (
+            <Square
+              type={"hover-square"}
+              square={hoverSquare.rank.toString() + hoverSquare.file.toString()}
+            ></Square>
+          ) : null}
+
+          {activeSquare ? (
+            <Square
+              type={"highlight"}
+              square={
+                activeSquare.hex.rank.toString() +
+                activeSquare.hex.file.toString()
+              }
+              style={{ backgroundColor: "rgba(255, 255, 0)" }}
+            ></Square>
+          ) : null}
+
+          {legalMoves?.map((square) => {
+            return (
+              <Square
+                type={square.type}
+                square={square.rank.toString() + square.file.toString()}
+                key={square.rank.toString() + square.file.toString()}
+              ></Square>
+            );
+          })}
+        </div>
+        <div className={"resizer"} onMouseDown={initResize}></div>
       </div>
-      <div className={"resizer"} onMouseDown={initResize}></div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default Board;
---
+++ src/demos/LearnWithVideo.js
@@ -0,0 +1,117 @@
+import * as React from "react";
+import "../App.css";
+import "../css/learnwithvideo.css";
+import Chess from "chess.js";
+
+import { playSound } from "../utils/utils";
+import { constructPgnTree, getGoodMoves } from "../utils/pgnHelper";
+import { hexToSan } from "../utils/helper";
+import Board from "../components/Board";
+import PgnViewer from "../components/PgnViewer";
+import openings from "../pgns/youtube";
+
+function App() {
+  const opening = openings["A45"];
+  /* States */
+  /* ************ */
+  const [game, setGame] = React.useState(new Chess());
+  const [currentNode, setCurrentNode] = React.useState(
+    constructPgnTree(opening.pgn)
+  );
+
+  /* Refs & Derived State */
+  /* ************ */
+  const boardRef = React.useRef();
+
+  function handleMove(hexmove) {
+    const move = game.move({
+      from: hexToSan(hexmove.from.rank, hexmove.from.file),
+      to: hexToSan(hexmove.to.rank, hexmove.to.file),
+    });
+
+    boardRef.current.setBoard(game.board());
+    move.flags = move.san.includes("+") ? move.flags + "+" : move.flags;
+    playSound(move.flags);
+    if (move) {
+      const goodMoves = getGoodMoves(currentNode);
+      const node = goodMoves.find((e) => e.move === move.san);
+      const moveRecommended = node ? true : false;
+
+      if (moveRecommended) {
+        setGame(game);
+        setCurrentNode(node);
+      } else {
+        setTimeout(() => {
+          game.undo();
+          boardRef.current.setBoard(game.board());
+        }, 777);
+      }
+    }
+  }
+
+  function goToNode(node) {
+    console.log("go to:", node);
+    let path = [];
+    const endNode = node;
+
+    while (node.move) {
+      path.push(node.move);
+      node = node.parent;
+    }
+    console.log(path);
+    const newGame = new Chess();
+    while (path.length) {
+      newGame.move(path.pop());
+    }
+    setGame(newGame);
+    setCurrentNode(endNode);
+    boardRef.current.setBoard(newGame.board());
+  }
+
+  React.useEffect(() => {
+    if (currentNode.nextMove === null) {
+      return;
+    }
+    if (game.turn() !== opening.color[0]) {
+      const goodMoves = getGoodMoves(currentNode);
+      const node = goodMoves[Math.floor(Math.random() * goodMoves.length)];
+
+      setTimeout(() => {
+        game.move(node.move);
+        setCurrentNode(node);
+        setGame(game);
+        boardRef.current.setBoard(game.board());
+      }, 777);
+    }
+  }, [game, opening, currentNode]);
+
+  return (
+    <div id="main">
+      <div id="appgame">
+        <Board ref={boardRef} onMakeMove={handleMove}></Board>
+      </div>
+      <div>
+        <div className="flex-column">
+          <iframe
+            id="yt-player"
+            src={"https://www.youtube.com/embed/" + opening.youtube}
+            style={{ height: "35vh", width: "calc(35vh*(16/9))" }}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+
+          <div id={"opening-title"}>{opening.title}</div>
+          <PgnViewer
+            tree={currentNode}
+            currentNode={currentNode}
+            goToNode={goToNode}
+          ></PgnViewer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
